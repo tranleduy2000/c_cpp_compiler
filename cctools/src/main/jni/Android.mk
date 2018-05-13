081ab553198aa1828f6b2b922f691db2f8a52cc0
Duy <tranleduy1233@gmail.com>
2018-05-14 00:48:47 +0700

    fix wrong path Android.mk

--- cctools/src/main/jni/Android.mk
+++ cctools/src/main/jni/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-
+ROOT_PATH  := $(LOCAL_PATH)
 #include $(LOCAL_PATH)/unzip11/Android.mk
 
-include utils/Android.mk
-include native-loader/Android.mk
+include $(ROOT_PATH)/utils/Android.mk
+include $(ROOT_PATH)/native-loader/Android.mk
