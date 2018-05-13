16aa4fcdf106c7c0712632ad6a27422d062bccde
Duy <tranleduy1233@gmail.com>
2018-05-14 00:32:22 +0700

    config plugin sdl success

---
+++ sdlplugin/README.md
@@ -0,0 +1,6 @@
+## SLD plugin
+This module must be build with ``android-ndk-r12b``, newer version will be give error.
+
+1. Download ``NDK 12`` at https://developer.android.com/ndk/downloads/revision_history
+2. Goto ``local.properties`` and set NDK dir ``ndk.dir=path_to_ndk\\android-ndk-r12b``
+3. Build
--- sdlplugin/src/main/jni/Android.mk
+++ sdlplugin/src/main/jni/Android.mk
@@ -1,11 +1,14 @@
-$(call import-module SDL)
-# include SDL/Android.mk
-# include SDL2_image/external/libwebp-0.3.0/Android.mk
-# include SDL2_image/Android.mk
-# include SDL2_mixer/external/libmikmod-3.1.12/Android.mk
-# include SDL2_mixer/external/smpeg2-2.0.0/Android.mk
-# include SDL2_mixer/Android.mk
-# include SDL2_net/Android.mk
-# include SDL2_ttf/Android.mk
-# include src/Android.mk
-# include utils/Android.mk
+LOCAL_PATH := $(call my-dir)
+ROOT_PATH := ${LOCAL_PATH}
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/SDL/Android.mk
+
+include $(ROOT_PATH)/SDL2_image/external/libwebp-0.3.0/Android.mk
+include $(ROOT_PATH)/SDL2_image/Android.mk
+include $(ROOT_PATH)/SDL2_mixer/external/libmikmod-3.1.12/Android.mk
+include $(ROOT_PATH)/SDL2_mixer/external/smpeg2-2.0.0/Android.mk
+include $(ROOT_PATH)/SDL2_mixer/Android.mk
+include $(ROOT_PATH)/SDL2_net/Android.mk
+include $(ROOT_PATH)/SDL2_ttf/Android.mk
+include $(ROOT_PATH)/src/Android.mk
+include $(ROOT_PATH)/utils/Android.mk
