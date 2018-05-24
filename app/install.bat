d844c72195ac4a9489eca913b4edf79379e7bd87
Duy <tranleduy1233@gmail.com>
2018-05-24 14:58:22 +0700

    Fix missing resource data

--- app/install.bat
+++ app/install.bat
@@ -1,6 +1,4 @@
 cd app
-cp build\outputs\apk\release\app-release.apk release/app-release.apk
-adb uninstall com.duy.ccppcompiler
-adb install -r release/app-release.apk
-adb shell am start -n "com.duy.ccppcompiler/com.duy.ccppcompiler.InstallActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-exit
+adb uninstall com.duy.c.cpp.compiler
+adb install -r arch_arm7/release/app-arch_arm7-release.apk
+adb shell am start -n "com.duy.c.cpp.compiler/com.duy.ccppcompiler.filemanager.install.InstallActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
