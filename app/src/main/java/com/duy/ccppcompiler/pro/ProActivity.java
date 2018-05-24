980b8bf3a5fb54460f8a32bbd1ec858a20c674bc
Duy <tranleduy1233@gmail.com>
2018-05-24 13:56:20 +0700

    Integrate in app purchase

--- app/src/main/java/com/duy/ccppcompiler/pro/ProActivity.java
+++
@@ -1,6 +0,0 @@
-package com.duy.ccppcompiler.pro;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class ProActivity extends AppCompatActivity {
-}
---
+++ app/src/main/java/com/duy/common/io/IoUtils.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2018 by Tran Le Duy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duy.common.io;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * Created by Duy on 12-Apr-18.
+ */
+
+public class IoUtils {
+
+    public static String toString(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "UTF-8");
+        for (; ; ) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+    }
+
+    public static void delete(File file) {
+        if (!file.exists()) {
+            return;
+        }
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (file != null) {
+                for (File f : files) {
+                    delete(f);
+                }
+            }
+        } else if (file.isFile()) {
+            file.delete();
+        }
+    }
+
+    public static void deleteChild(File toDelete, FileFilter fileFilter) {
+        File[] files = toDelete.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (fileFilter.accept(file)) {
+                    file.delete();
+                }
+            }
+        }
+    }
+}
