95d224487a586898b918670148fa38cb02af0e4e
duy <tranleduy1233@gmail.com>
2018-05-27 22:55:40 +0700

    refactor code

---
+++ app/src/main/java/com/duy/CppIdeApplication.java
@@ -0,0 +1,14 @@
+
+package com.duy;
+
+import android.support.multidex.MultiDexApplication;
+
+
+public class CppIdeApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
