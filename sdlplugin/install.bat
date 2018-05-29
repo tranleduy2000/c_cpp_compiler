3fe3d8fae30d2aaa12b778c14e78231949dfe90e
Duy <tranleduy1233@gmail.com>
2018-05-29 23:20:18 +0700

    SDL activity exported, add screenshots

--- sdlplugin/install.bat
+++ sdlplugin/install.bat
@@ -1,3 +1,4 @@
 adb uninstall com.duy.c.cpp.compiler.sdlplugin
 adb install -r ./build/outputs/apk/release/sdlplugin-release.apk
 adb shell am start -n "com.duy.c.cpp.compiler.sdlplugin/com.duy.c.cpp.compiler.sdlplugin.sdlpluginActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+exit
