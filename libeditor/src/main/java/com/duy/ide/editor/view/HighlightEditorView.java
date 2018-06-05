f1f9073958987e758dba76e253331a1ef69d741a
Duy <tranleduy1233@gmail.com>
2018-06-05 23:04:04 +0700

    Fixed wrong line number when chang text sizze

--- libeditor/src/main/java/com/duy/ide/editor/view/HighlightEditorView.java
+++ libeditor/src/main/java/com/duy/ide/editor/view/HighlightEditorView.java
@@ -331,6 +331,9 @@ public abstract class HighlightEditorView extends android.support.v7.widget.AppC
         super.setTextSize(unit, size);
         updateLayoutContext();
         updateTabChar();
+
+        mNeedUpdateLineNumber = true;
+        mPreLineCount = -1;
     }
 
     @Override
